452584ac4af52714c2793d050455a5db6d2f0e36
smolbun <45829953+smolbun@users.noreply.github.com>
2024-05-27 08:40:48 +0800

    Update CC1101_Frequency_Analyzer.ino

--- CC1101_Frequency_Analyzer/CC1101_Frequency_Analyzer.ino
+++ CC1101_Frequency_Analyzer/CC1101_Frequency_Analyzer.ino
@@ -12,28 +12,13 @@
 CC1101 radio = new Module(SS_PIN, gdo0, RADIOLIB_NC, gdo2);
 
 static const uint32_t subghz_frequency_list[] = {
-  /* 300 - 348 */
-  300000000,
-  303875000,
-  304250000,
-  310000000,
-  315000000,
-  318000000,
-
-  /* 387 - 464 */
-  390000000,
-  418000000,
-  433075000,
-  433420000,
-  433920000,
-  434420000,
-  434775000,
-  438900000,
-
-  /* 779 - 928 */
-  868350000,
-  915000000,
-  925000000
+  300000000, 302757000, 303875000, 303900000, 304250000, 307000000, 307500000, 307800000,
+  309000000, 310000000, 312000000, 312100000, 313000000, 313850000, 314000000, 314350000,
+  314980000, 315000000, 318000000, 330000000, 345000000, 348000000, 350000000, 387000000,
+  390000000, 418000000, 430000000, 431000000, 431500000, 433075000, 433220000, 433420000,
+  433657070, 433889000, 433920000, 434075000, 434176948, 434390000, 434420000, 434775000,
+  438900000, 440175000, 464000000, 779000000, 868350000, 868400000, 868800000, 868950000,
+  906400000, 915000000, 925000000, 928000000
 };
 
 typedef struct
@@ -44,7 +29,7 @@ typedef struct
   int rssi_fine;
 } FrequencyRSSI;
 
-const int rssi_threshold = -90;
+const int rssi_threshold = -78;
 
 void setup() {
   Serial.begin(115200);
@@ -60,46 +45,48 @@ void loop() {
   };
 
   // First stage: coarse scan
-  // Run through common frequency presets to get the most accurate coarse frequency
+  radio.setRxBandwidth(650);
   size_t array_size = sizeof(subghz_frequency_list) / sizeof(subghz_frequency_list[0]);
   for (size_t i = 0; i < array_size; i++) {
     uint32_t frequency = subghz_frequency_list[i];
-    radio.setRxBandwidth(650);
-    radio.setFrequency((float)frequency / 1000000.0);
-    radio.receiveDirect();
-    delay(3);
-    rssi = radio.getRSSI();
-
-    if (frequency_rssi.rssi_coarse < rssi) {
-      frequency_rssi.rssi_coarse = rssi;
-      frequency_rssi.frequency_coarse = frequency;
-    }
-  }
-
-
-  // Second stage: fine scan
-  if (frequency_rssi.rssi_coarse > rssi_threshold) {
-    // for example -0.3 ... 433.92 ... +0.3 step 20KHz
-    for (uint32_t i = frequency_rssi.frequency_coarse - 300000; i < frequency_rssi.frequency_coarse + 300000; i += 20000) {
-      uint32_t frequency = i;
-      radio.setRxBandwidth(58);
+    if (frequency != 467750000 && frequency != 464000000 && frequency != 390000000 && frequency != 312000000 && frequency != 312100000 && frequency != 312200000 && frequency != 440175000) {
       radio.setFrequency((float)frequency / 1000000.0);
       radio.receiveDirect();
-      delay(3);
+      delay(2);
       rssi = radio.getRSSI();
 
-      if (rssi >= frequency_rssi.rssi_coarse) {
-        if (frequency_rssi.rssi_fine < rssi) {
-          frequency_rssi.rssi_fine = rssi;
-          frequency_rssi.frequency_fine = frequency;
-        }
+      if (frequency_rssi.rssi_coarse < rssi) {
+        frequency_rssi.rssi_coarse = rssi;
+        frequency_rssi.frequency_coarse = frequency;
       }
     }
   }
 
-  if (frequency_rssi.frequency_fine != 0) {
-    // The frequency out would be approximately ±0.02 of the true frequency
-    Serial.printf("1. COARSE      Frequency: %.2f  RSSI: %d\n", (float)frequency_rssi.frequency_coarse / 1000000.0, frequency_rssi.rssi_coarse);
-    Serial.printf("2. FINE        Frequency: %.2f  RSSI: %d\n\n", (float)frequency_rssi.frequency_fine / 1000000.0, frequency_rssi.rssi_fine);
+  // Second stage: fine scan
+  if (frequency_rssi.rssi_coarse > rssi_threshold) {
+    // for example -0.3 ... 433.92 ... +0.3 step 20KHz
+    radio.setRxBandwidth(58);
+    for (uint32_t i = frequency_rssi.frequency_coarse - 300000; i < frequency_rssi.frequency_coarse + 300000; i += 20000) {
+      uint32_t frequency = i;
+      radio.setFrequency((float)frequency / 1000000.0);
+      radio.receiveDirect();
+      delay(2);
+      rssi = radio.getRSSI();
+
+      if (frequency_rssi.rssi_fine < rssi) {
+        frequency_rssi.rssi_fine = rssi;
+        frequency_rssi.frequency_fine = frequency;
+      }
+    }
+  }
+
+  // Deliver results fine
+  if (frequency_rssi.rssi_fine > rssi_threshold) {
+    Serial.printf("FINE        Frequency: %.2f  RSSI: %d\n", (float)frequency_rssi.frequency_fine / 1000000.0, frequency_rssi.rssi_fine);
+  }
+
+  // Deliver results coarse
+  else if (frequency_rssi.rssi_coarse > rssi_threshold) {
+    Serial.printf("COARSE      Frequency: %.2f  RSSI: %d\n", (float)frequency_rssi.frequency_coarse / 1000000.0, frequency_rssi.rssi_coarse);
   }
 }
